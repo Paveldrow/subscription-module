5e3f34210f964bc66c60f3c3c6f3a4eb609c3c87
Pavel Drow <paveldrow@yandex.ru>
2021-04-07 16:36:37 +0300

    added validation email

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-const emailSubscription = document.querySelector('form');
+const emailForm = document.querySelector('.form');
+const emailInput = document.querySelector('.form__email-input')
 
-emailSubscription.addEventListener('submit', function (evt) {
+
+emailForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
+  const reg = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+  const email = emailInput.value;
+  const valid = reg.test(email);
+  if (valid) {
+  emailInput.classList.remove('form__email-input--invalid')
+  emailForm.classList.remove('form--invalid');
+  } else {
+    emailInput.classList.add('form__email-input--invalid')
+    emailForm.classList.add('form--invalid');
+  }
 });
+  
+
+
+// const validEmail = () => {
+//   const email = emailInput.value;
+//   console.log(email);
+
+//   emailInput.reportValidity();
+// };
+
+// emailSubscription.addEventListener('input', () => {
+//   const valueLength = emailSubscription.value.length;
+
+//   if ( valueLength < MIN_EMAIL_LENGTH) {
+//     emailSubscription.setCustomValidity('Check if the email is correct');
+//   } else {
+//     emailSubscription.setCustomValidity('');
+//   }
+//   emailSubscription.reportValidity();
+// });
+
+// const submitEmail = (emailInput) => {
+//   emailInput.addEventListener('input', (evt) => {
+//     console.log(emailInput.value)
+//   })
+// };
+
+
